a2eefeb13ae936a20aed96c793c6737d0aedd687
azoom-tran-quoc-thien <tran.quoc.thien@azoom.jp>
2023-09-08 08:41:31 +0700

    :+1: Add route `accounts/post.js, accounts/get.js`

--- services/sms.js
+++ services/sms.js
@@ -1,3 +1,9 @@
-export const sendSMS = (tel, message) => {
+export const sendSMS = async (tel, message) => {
   console.log('Sending SMS to', tel, 'with message:', message)
+  if (process.env.NODE_ENV === 'test') {
+    throw new Error('SMS service is not available in test mode')
+  } else {
+    await new Promise(resolve => setTimeout(resolve, 1000))
+    console.log('SMS sent to', tel, 'with message:', message)
+  }
 }
