081c8311fe391a70853983ca378c35d270586d86
azoom-tran-quoc-thien <tran.quoc.thien@azoom.jp>
2023-09-13 18:40:50 +0700

    🔨 Fix get accounts

--- routes/accounts/get.js
+++ routes/accounts/get.js
@@ -5,15 +5,12 @@ export { default as middleware } from '@middleware/ensure-authenticated'
  * @type {import('express').RequestHandler}
  */
 export default async (req, res) => {
-  const { page = 1, sortById = 'ASC', limit = 10 } = req.query
+  const { page = 1, limit = 10 } = req.query
   const skip = +limit && +page && page > 0 ? (page - 1) * limit : undefined
   const [data, total] = await Promise.all([
     prisma.account.findMany({
       where: {
         customerId: req.user.id,
-      },
-      orderBy: {
-        id: sortById.toLowerCase(),
       },
       skip: skip ? skip : undefined,
       take: +limit ? +limit : undefined,
