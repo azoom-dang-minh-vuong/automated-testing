754de9d97f3aa14ac88ed10768554ff5e8ef3ab5
azoom-tran-quoc-thien <tran.quoc.thien@azoom.jp>
2023-09-13 18:15:40 +0700

    :+1: Add test for post transactions

--- tests/routes/transactions/post.test.js
+++ tests/routes/transactions/post.test.js
@@ -21,12 +21,22 @@ const request = (userId, body) => {
     .send(body)
 }
 
+let transactionTypes
+beforeAll(async () => {
+  transactionTypes = await transactionTypeFactory.createList(
+    Object.values(TRANSACTION_TYPES).map(type => ({
+      type,
+    }))
+  )
+})
+
 afterAll(async () => {
+  await deleteFixtures()
   await prisma.$disconnect()
 })
 
 describe('POST /transactions', () => {
-  let users, accounts, mockSendSMS, resetSendSMS, transactionTypes
+  let users, accounts, mockSendSMS, resetSendSMS
   beforeEach(async () => {
     users = await customerFactory.createList(2)
     accounts = await accountFactory.createList(
@@ -39,17 +49,11 @@ describe('POST /transactions', () => {
         amount: 100000,
       }))
     )
-    transactionTypes = await transactionTypeFactory.createList(
-      Object.values(TRANSACTION_TYPES).map(type => ({
-        type,
-      }))
-    )
     mockSendSMS = jest.fn()
     resetSendSMS = post.__set__('sendSMS', mockSendSMS)
   })
   afterEach(async () => {
     resetSendSMS()
-    await deleteFixtures()
   })
   describe('normal scenario', () => {
     test('should create new transaction when transactionType is TRANSFER', async () => {
@@ -94,142 +98,8 @@ describe('POST /transactions', () => {
         [users.find(({ id }) => id === receiverAccount.customerId).tel, expect.any(String)],
       ])
     })
-    test('should create new transaction when transactionType is WITHDRAWAL', async () => {
-      const user = faker.helpers.arrayElement(users)
-      const transactionType = transactionTypes.find(
-        ({ type }) => type === TRANSACTION_TYPES.WITHDRAWAL
-      )
-      const account = faker.helpers.arrayElement(
-        accounts.filter(account => account.customerId === user.id)
-      )
-      const reqBody = {
-        senderAccountNumber: account.accountNumber.toString(),
-        amount: faker.number.int({ max: 40000 }),
-        message: 'test',
-      }
-      await request(user.id, reqBody).expect(201)
-      const resultAccount = await prisma.account.findUnique({
-        where: { accountNumber: reqBody.senderAccountNumber },
-      })
-      expect(resultAccount.amount).toBe(account.amount - reqBody.amount - transactionType.fee)
-      expect(mockSendSMS.mock.calls).toEqual([[user.tel, expect.any(String)]])
-    })
-    test('should create new transaction when transactionType is DEPOSIT', async () => {
-      const user = faker.helpers.arrayElement(users)
-      const transactionType = transactionTypes.find(
-        ({ type }) => type === TRANSACTION_TYPES.DEPOSIT
-      )
-      const account = faker.helpers.arrayElement(
-        accounts.filter(account => account.customerId === user.id)
-      )
-      const reqBody = {
-        receiverAccountNumber: account.accountNumber.toString(),
-        amount: faker.number.int({ max: 40000 }),
-        message: 'test',
-      }
-      await request(user.id, reqBody).expect(201)
-      const resultAccount = await prisma.account.findUnique({
-        where: { accountNumber: reqBody.receiverAccountNumber },
-      })
-      expect(resultAccount.amount).toBe(account.amount + reqBody.amount - transactionType.fee)
-      expect(mockSendSMS.mock.calls).toEqual([[user.tel, expect.any(String)]])
-    })
   })
   describe('exception scenario', () => {
-    test('should return 400 when senderAccountNumber is invalid', async () => {
-      const reqBody = {
-        senderAccountNumber: faker.string.alpha(10),
-        receiverAccountNumber: accounts[1].accountNumber.toString(),
-        amount: faker.number.int({ max: 40000 }),
-        message: 'test',
-      }
-      await request(faker.helpers.arrayElement(users).id, reqBody).expect(400)
-    })
-    test('should return 400 when receiverAccountNumber is invalid', async () => {
-      const user = faker.helpers.arrayElement(users)
-      const account = faker.helpers.arrayElement(
-        accounts.filter(account => account.customerId === user.id)
-      )
-      const reqBody = {
-        senderAccountNumber: account.accountNumber,
-        receiverAccountNumber: faker.string.alpha(10),
-        amount: faker.number.int({ max: 40000 }),
-        message: 'test',
-      }
-      await request(user.id, reqBody).expect(400)
-    })
-    test('should return 400 when senderAccountNumber is equal receiverAccountNumber', async () => {
-      const user = faker.helpers.arrayElement(users)
-      const account = faker.helpers.arrayElement(
-        accounts.filter(account => account.customerId === user.id)
-      )
-      const reqBody = {
-        senderAccountNumber: account.accountNumber,
-        receiverAccountNumber: account.accountNumber,
-        amount: faker.number.int({ max: 40000 }),
-        message: 'test',
-      }
-      await request(user.id, reqBody).expect(400)
-    })
-    test('should return 400 when amount is not valid', async () => {
-      const user = faker.helpers.arrayElement(users)
-      const senderAccount = faker.helpers.arrayElement(
-        accounts.filter(account => account.customerId === user.id)
-      )
-      const receiverAccount = faker.helpers.arrayElement(
-        accounts.filter(({ id }) => id !== senderAccount.id)
-      )
-      const reqBody = {
-        senderAccountNumber: senderAccount.accountNumber,
-        receiverAccountNumber: receiverAccount.accountNumber,
-        amount: -1,
-        message: 'test',
-      }
-      await request(user.id, reqBody).expect(400)
-    })
-    test('should return 400 when senderAccount is not found and transactionType is WITHDRAWAL', async () => {
-      const reqBody = {
-        senderAccountNumber: faker.string.numeric(10),
-        amount: faker.number.int({ max: 40000 }),
-        message: 'test',
-      }
-      await request(faker.helpers.arrayElement(users).id, reqBody).expect(400)
-    })
-    test('should return 400 when senderAccount amount is not enough and transactionType is WITHDRAWAL', async () => {
-      const user = faker.helpers.arrayElement(users)
-      const account = faker.helpers.arrayElement(
-        accounts.filter(account => account.customerId === user.id)
-      )
-      const reqBody = {
-        senderAccountNumber: account.accountNumber,
-        amount: account.amount ** 10,
-        message: 'test',
-      }
-      await request(user.id, reqBody).expect(400)
-    })
-    test('should return 400 when senderAccount is not found and transactionType is TRANSFER', async () => {
-      const user = faker.helpers.arrayElement(users)
-      const reqBody = {
-        senderAccountNumber: faker.string.numeric(10),
-        receiverAccountNumber: faker.helpers.arrayElement(accounts).accountNumber,
-        amount: faker.number.int({ max: 40000 }),
-        message: 'test',
-      }
-      await request(user.id, reqBody).expect(400)
-    })
-    test('should return 400 when receiverAccount is not found and transactionType is TRANSFER', async () => {
-      const user = faker.helpers.arrayElement(users)
-      const account = faker.helpers.arrayElement(
-        accounts.filter(account => account.customerId === user.id)
-      )
-      const reqBody = {
-        senderAccountNumber: account.accountNumber,
-        receiverAccountNumber: faker.string.numeric(10),
-        amount: faker.number.int({ max: 40000 }),
-        message: 'test',
-      }
-      await request(user.id, reqBody).expect(400)
-    })
     test('should return 400 when senderAccount amount is not enough and transactionType is TRANSFER', async () => {
       const user = faker.helpers.arrayElement(users)
       const senderAccount = faker.helpers.arrayElement(
@@ -245,31 +115,6 @@ describe('POST /transactions', () => {
         message: 'test',
       }
       await request(user.id, reqBody).expect(400)
-    })
-    test('should return 401 if token is not provided', async () => {
-      await client
-        .post('/transactions')
-        .send({
-          senderAccountNumber: faker.string.numeric(10),
-          receiverAccountNumber: faker.string.numeric(10),
-          amount: faker.number.int({ max: 40000 }),
-          message: 'test',
-        })
-        .expect(401)
-    })
-
-    test('should return 401 if token is invalid', async () => {
-      const invalidToken = generate(0)
-      await client
-        .post('/transactions')
-        .set('Authorization', `Bearer ${invalidToken}`)
-        .send({
-          senderAccountNumber: faker.string.numeric(10),
-          receiverAccountNumber: faker.string.numeric(10),
-          amount: faker.number.int({ max: 40000 }),
-          message: 'test',
-        })
-        .expect(401)
     })
   })
 })
